9bfaaf7dc928611052c18e2ae053ce89fc74d07e
Sin <syain1310@gmail.com>
2017-05-25 21:28:42 +0900

    처음커밋

---
+++ ZenFurniture/src/main/java/zen/furniture/ZenFurnitureApplication.java
@@ -0,0 +1,11 @@
+package zen.furniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenFurnitureApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZenFurnitureApplication.class, args);
+	}
+}
---
+++ ZenFurniture/src/main/java/zen/furniture/web/spring/WebMvcConfig.java
@@ -0,0 +1,34 @@
+package zen.furniture.web.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public DispatcherServlet dispatcherServlet(){
+		return new DispatcherServlet();
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("login");
+	}
+	
+	@Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+}
